b93b899cbebbdb87053f8a60d81920038557b0d9
MateusProDev <mateusferreiraprodev@gmail.com>
2024-03-17 11:57:33 -0300

    Add

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
         throw new Error('Error ao buscar dados do perfil do github');
     }
     return response.json();
-})
-.then(data => {
+})*/
+/*.then(data => {
     console.log(data)
 
     const avatarUrl = data.avatar_url;
@@ -31,3 +31,39 @@ lines.addEventListener('click', () => {
   lines.classList.toggle('active');
   navLinks.classList.toggle('active');
 });
+
+
+// Função para adicionar animações quando um elemento entra no viewport
+function animateOnScroll(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+            observer.unobserve(entry.target); // Parar de observar o elemento após a animação ser aplicada
+        }
+    });
+}
+
+// Criar um observador para animar os elementos quando entrarem no viewport
+const observer = new IntersectionObserver(animateOnScroll, { threshold: 0.5 });
+
+// Selecionar todos os elementos que serão animados
+const elementsToAnimate = document.querySelectorAll('.animate-slide-in');
+
+// Registrar cada elemento no observador
+elementsToAnimate.forEach(element => {
+    observer.observe(element);
+});
+
+// Ativar as animações para os elementos que já estão visíveis na página
+animateOnScroll(elementsToAnimate.map(element => ({ target: element, isIntersecting: isElementInViewport(element) })), observer);
+
+// Função para verificar se um elemento está visível no viewport
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
